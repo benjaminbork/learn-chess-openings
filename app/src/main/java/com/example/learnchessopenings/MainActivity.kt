b24c8b87939576da799ad8cb6030777c81a5434c
benjaminbork <benjamin.bork@schifferberg.com>
2023-01-16 23:18:06 +0100

    refactored bottom_navigation_bar and created inital main_app_bar

--- app/src/main/java/com/example/learnchessopenings/MainActivity.kt
+++ app/src/main/java/com/example/learnchessopenings/MainActivity.kt
@@ -5,16 +5,20 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.learnchessopenings.databinding.ActivityMainBinding
+import com.example.learnchessopenings.databinding.BottomNavigationBarBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
-        binding.bottomNavigationView.setOnItemSelectedListener {
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.home -> replaceFragment(Home())
                 R.id.courses -> replaceFragment(Courses())
@@ -32,4 +36,5 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frameLayout,fragment)
         fragmentTransaction.commit()
     }
+
 }
