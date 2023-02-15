86c86cefc9e677365c07ec9c3ee72bb9c513c741
Chrisoman <c.meyer@student.xu-university.de>
2023-02-15 03:05:49 +0100

    Set up course detail card

--- app/src/main/java/com/example/learnchessopenings/DetailedCourse.kt
+++ app/src/main/java/com/example/learnchessopenings/DetailedCourse.kt
@@ -1,19 +1,38 @@
 package com.example.learnchessopenings
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 
 class DetailedCourse() : AppCompatActivity() {
+    private var db: DbHelper = DbHelper(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.course_detail_card)
 
+        db = DbHelper(this)
+
         val data = intent.getIntExtra("id", 0)
-        Log.d("eee", data.toString())
+
+        populatePage(data)
+    }
+
+    private fun populatePage(courseId: Int) {
+
+        val data = getData(courseId)
+
+        val mainAppBar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.main_app_bar)
+        mainAppBar.title = ""
+    }
+
+    private fun getData(courseId: Int) {
+
+    }
+
+    override fun onDestroy() {
+        db.close()
+        super.onDestroy()
     }
 
 }
