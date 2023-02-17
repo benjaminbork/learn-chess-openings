08427cdf579d2e39c7c0fa13572fabd5e0664e17
Chrisoman <c.meyer@student.xu-university.de>
2023-02-17 13:28:47 +0100

    Fixed a merge mistake

--- app/src/main/java/com/example/learnchessopenings/Home.kt
+++ app/src/main/java/com/example/learnchessopenings/Home.kt
@@ -3,8 +3,6 @@ package com.example.learnchessopenings
 import android.content.Intent
 import android.icu.util.Calendar
 import android.os.Bundle
-import android.provider.BaseColumns
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -58,6 +56,15 @@ class Home : Fragment()  {
         return homeView
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<CardView>(R.id.dailyPuzzleCard).setOnClickListener {
+            val puzzle = Intent(context, PuzzleActivity::class.java)
+            puzzle.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(puzzle)
+        }
+    }
+
 
     private fun populateRecycler(homeView: View) {
         val dashboardRecycler = homeView.findViewById<RecyclerView>(R.id.dashboardRecycler)
@@ -89,14 +96,6 @@ class Home : Fragment()  {
         }
 
         dashboardRecycler.adapter = dashboardAdapter(data)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        view.findViewById<CardView>(R.id.dailyPuzzleCard).setOnClickListener{
-            val puzzle = Intent (context, PuzzleActivity::class.java)
-            puzzle.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(puzzle)
-        }
 
     }
 
